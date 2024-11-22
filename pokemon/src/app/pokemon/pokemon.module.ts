82624a5954f80e5aa84ce134d9c119b03e725e67
Juliana0203 <j.morenon2@uniandes.edu.co>
2024-11-22 07:04:26 -0500

    Primeros cambios

--- pokemon/src/app/pokemon/pokemon.module.ts
+++ pokemon/src/app/pokemon/pokemon.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [PokemonListComponent],
+  declarations: [PokemonListComponent, PokemonDetailComponent],
   exports: [PokemonListComponent],
 })
 export class PokemonModule {}
